faab4c4117b82324e2da62be2f89b2d2f9667e29
Raphael Mastel III <s179135@students.aie.edu.au>
2018-01-17 17:52:00 -0800

    working player character

---
+++ Tempo time/Assets/Scripts/playerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class playerController : NetworkBehaviour {
+
+    public float forceMultiplier = 5;
+
+	void Update () {
+		if(!isLocalPlayer)
+        {
+            return;
+        }
+
+        float x = Input.GetAxis("Horizontal") * forceMultiplier;
+        float y = Input.GetAxis("Vertical") * forceMultiplier;
+
+        if(GetComponent<Rigidbody>().velocity.magnitude > 0.5f)
+        transform.LookAt(GetComponent<Rigidbody>().velocity + transform.position);
+        GetComponent<Rigidbody>().AddForce(x,0,y);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+	}
+
+    void Collision()
+    {
+
+    }
+}
