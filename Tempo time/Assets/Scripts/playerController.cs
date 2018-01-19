d78667d33c4ca9f44e6d511dc7c19a24ca5cb91d
Raphael Mastel III <s179135@students.aie.edu.au>
2018-01-18 17:38:21 -0800

    mostly working player controller

--- Tempo time/Assets/Scripts/playerController.cs
+++ Tempo time/Assets/Scripts/playerController.cs
@@ -6,6 +6,9 @@ using UnityEngine.Networking;
 public class playerController : NetworkBehaviour {
 
     public float forceMultiplier = 5;
+    public float jumpForce = 20;
+    public float push = 10;
+    public bool hasJumped = false;
 
 	void Update () {
 		if(!isLocalPlayer)
@@ -13,21 +16,31 @@ public class playerController : NetworkBehaviour {
             return;
         }
 
+        //////////Movement**************************************
         float x = Input.GetAxis("Horizontal") * forceMultiplier;
         float y = Input.GetAxis("Vertical") * forceMultiplier;
 
         if(GetComponent<Rigidbody>().velocity.magnitude > 0.5f)
-        transform.LookAt(GetComponent<Rigidbody>().velocity + transform.position);
+        transform.LookAt(GetComponent<Rigidbody>().velocity + transform.position - new Vector3(0, GetComponent<Rigidbody>().velocity.y, 0));
         GetComponent<Rigidbody>().AddForce(x,0,y);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && hasJumped == false)
         {
-
+            GetComponent<Rigidbody>().AddForce(0,jumpForce,0);
+            hasJumped = true;
         }
+        //////////////////////////////////////////////////////////////////////
+
+        //////////Pushing******************************************
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody>().AddExplosionForce(push, transform.position + GetComponent<Rigidbody>().velocity.normalized, 1);
+        }
+        //////////////////////////////////////////////////////////////////////
 	}
 
-    void Collision()
+    void OnCollisionEnter()
     {
-
+            hasJumped = false;
     }
 }
