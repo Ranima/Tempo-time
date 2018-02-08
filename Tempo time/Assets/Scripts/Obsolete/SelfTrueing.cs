b4658004ff342fbc7ef6881a20401130859d0d6a
Raphael Mastel III <s179135@students.aie.edu.au>
2018-02-07 17:21:29 -0800

    Rewired is added and installed

---
+++ Tempo time/Assets/Scripts/Obsolete/SelfTrueing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfTrueing : MonoBehaviour {
+
+    public bool isStanding = true;
+    public float rot;
+    public float rotationCheck;
+    private Rigidbody rigid;
+
+    void Start () {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+	void Update () {
+        if (isStanding)
+        {
+            if (transform.rotation.x != 0)
+            {
+                rigid.AddTorque(rot, 0, 0);
+            }
+            if (transform.rotation.z != 0)
+            {
+                rigid.AddTorque(0, 0, rot);
+            }
+        }
+	}
+}
